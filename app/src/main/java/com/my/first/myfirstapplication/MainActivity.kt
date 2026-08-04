6ddb61abe5b05772c71ce29ed4dc399ff4ca3172
C-Rodriguez-cmyk <rc.caleb18@gmail.com>
2026-08-04 10:07:16 -0600

    Toast

--- app/src/main/java/com/my/first/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/my/first/myfirstapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.my.first.myfirstapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val boton = findViewById<Button>(R.id.boton_enviar_dato)
+        boton.setOnClickListener {
+            Toast.makeText(this, "esto es un toast", Toast.LENGTH_SHORT).show()
+        }
     }
 }
-
